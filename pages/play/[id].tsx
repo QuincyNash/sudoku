75e9fa34429d0f4ee0fe46ed22d756a69c75f13e
quincynash <quincy.nash@icloud.com>
2022-07-07 21:23:41 -0700

    Changes

--- pages/play/[id].tsx
+++ pages/play/[id].tsx
@@ -19,7 +19,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 		console.log();
 		admin.initializeApp({
 			credential: admin.credential.cert({
-				type: process.env.FIREBASE_TYPE,
+				type: "service_account",
 				project_id: process.env.FIREBASE_PROJECT_ID,
 				private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
 				private_key: (process.env.FIREBASE_PRIVATE_KEY as any).replace(
@@ -28,11 +28,12 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 				),
 				client_email: process.env.FIREBASE_CLIENT_EMAIL,
 				client_id: process.env.FIREBASE_CLIENT_ID,
-				auth_uri: process.env.FIREBASE_AUTH_URI,
-				token_uri: process.env.FIREBASE_TOKEN_URI,
+				auth_uri: "https://accounts.google.com/o/oauth2/auth",
+				token_uri: "https://oauth2.googleapis.com/token",
 				auth_provider_x509_cert_url:
-					process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-				client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+					"https://www.googleapis.com/oauth2/v1/certs",
+				client_x509_cert_url:
+					"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cipfz%40sudoku-dec29.iam.gserviceaccount.com",
 			} as any),
 			databaseURL: process.env.FIREBASE_DATABASE_URL,
 		});
